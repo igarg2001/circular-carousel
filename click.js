b46f720b690312091ec061e88446c5e6819b115b
igarg2001 <igarg2001@gmail.com>
2019-09-23 01:19:36 +0530

    commit

---
+++ click.js
@@ -0,0 +1,16 @@
+var p = document.querySelectorAll('.inner');
+let arr_name=['Event 1','Event 2','Event 3','Event 4'];
+let arr_desc=['This is the description for Event 1','This is the description for Event 2','This is the description for Event 3','This is the description for Event 4'];
+function click_event(n){
+    document.getElementById('event-name').innerHTML=''
+    document.getElementById('event-name').innerHTML=arr_name[n];
+    document.getElementById('event-desc').innerHTML='';
+    document.getElementById('event-desc').innerHTML=arr_desc[n];
+    document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
+    document.getElementsByClassName('events')[0].style.opacity = '1';
+}
+function closeEvents()
+{
+    document.getElementsByClassName('rotating-slider')[0].style.opacity = '1';
+    document.getElementsByClassName('events')[0].style.opacity = '0';   
+}
