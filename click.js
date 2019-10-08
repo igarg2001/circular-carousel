deff7e539f901c0b1db4efd8c17e9ff1bbe44bc8
Priyanshu <priyanshushukla9801@gmail.com>
2019-10-08 17:33:40 +0530

    contact us changes

--- click.js
+++ click.js
@@ -2,7 +2,7 @@ var p = document.querySelectorAll('.inner');
 let arr_name=['Event 1','Event 2','Event 3','Event 4','Event 5'];
 let arr_desc=['This is the description for Event 1','This is the description for Event 2','This is the description for Event 3','This is the description for Event 4','This is the description for Event 5'];
 function click_event(n){
-    let q=n+1
+    let q=n+1;
   
     document.getElementById('event-pic').style.backgroundImage ="url('neon'"+q+"'.jpg')";
     document.getElementById('event-name').innerHTML=''
@@ -10,12 +10,16 @@ function click_event(n){
     document.getElementById('event-desc').innerHTML='';
     document.getElementById('event-desc').innerHTML=arr_desc[n];
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
-    document.getElementsByClassName('events')[0].style.opacity = '1';
+    document.getElementById('events').style.opacity = '1';
+    console.log('dsvbjdsnv');
 }
 function closeEvents()
-{
+{   
+    console.log('yyss');
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '1';
-    document.getElementsByClassName('events')[0].style.opacity = '';   
+    console.log('hi');
+    document.getElementById('events').style.opacity = '0';
+    console.log('sefhsuf');
 }
 function openContacts() {
     document.getElementById('contacts').style.opacity = '1';
