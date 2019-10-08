df9acf91234672cba84ac21c42d40c8dfd79b683
igarg2001 <igarg2001@gmail.com>
2019-10-08 19:20:43 +0530

    nnn

--- click.js
+++ click.js
@@ -3,7 +3,7 @@ let arr_name=['Event 1','Event 2','Event 3','Event 4','Event 5'];
 let arr_desc=['This is the description for Event 1','This is the description for Event 2','This is the description for Event 3','This is the description for Event 4','This is the description for Event 5'];
 function click_event(n){
     let q=n+1;
-    console.log(document.getElementById('event-pic').innerHTML='<img src="neon'+q+'.jpg" style="height : 50px; width : 50px">');
+    document.getElementById('event-pic').innerHTML='<img src="neon'+q+'.jpg" style="height : 50px; width : 50px">';
     document.getElementById('event-name').innerHTML=''
     document.getElementById('event-name').innerHTML=arr_name[n];
     document.getElementById('event-desc').innerHTML='';
