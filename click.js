637ad12ec6e4766d98cba77c3594aea75c33162f
Priyanshu <priyanshushukla9801@gmail.com>
2019-10-08 14:01:35 +0530

    glowing and growing changes.

--- click.js
+++ click.js
@@ -1,13 +1,14 @@
 var p = document.querySelectorAll('.inner');
-let arr_name=['Event 1','Event 2','Event 3','Event 4'];
-let arr_desc=['This is the description for Event 1','This is the description for Event 2','This is the description for Event 3','This is the description for Event 4'];
+let arr_name=['Event 1','Event 2','Event 3','Event 4','Event 5'];
+let arr_desc=['This is the description for Event 1','This is the description for Event 2','This is the description for Event 3','This is the description for Event 4','This is the description for Event 5'];
 function click_event(n){
-    this.textContent = '';
-    document.getElementById('event-pic').innerHTML ='<img src="neon8.jpg" style="height : 50px; width : 50px">';
+    let q=n+1
+  
+    document.getElementById('event-pic').style.backgroundImage ="url('neon'"+q+"'.jpg')";
     document.getElementById('event-name').innerHTML=''
     document.getElementById('event-name').innerHTML=arr_name[n];
-    document.getElementById('event-info').innerHTML='';
-    document.getElementById('event-info').innerHTML=arr_desc[n];
+    document.getElementById('event-desc').innerHTML='';
+    document.getElementById('event-desc').innerHTML=arr_desc[n];
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
     document.getElementsByClassName('events')[0].style.opacity = '1';
 }
