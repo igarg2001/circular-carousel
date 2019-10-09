ee367feb6c0bc0d953a180b918e32ac50ecf6da4
Priyanshu <priyanshushukla9801@gmail.com>
2019-10-10 02:28:15 +0530

    event flags

--- click.js
+++ click.js
@@ -1,26 +1,32 @@
 var p = document.querySelectorAll('.inner');
 let arr_name=['Event 1','Event 2','Event 3','Event 4','Event 5'];
 let arr_desc=['The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense. <br> Event 1','The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense. <br> Event 2','The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense. <br> Event 3','The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense. <br> Event 4','The phrase "Lorem ipsum dolor sit amet consectetuer" appears in Microsoft Word online Help. This phrase has the appearance of an intelligent Latin idiom. Actually, it is nonsense. <br> Event 5'];
+let event_flag=true;
 function click_event(n){
     let q=n+1;
-    document.getElementById('event-pic').innerHTML='<img src="neon'+q+'.jpg" style="height : 50vh; width : 35vw; border-radius : 15px;">';
-    document.getElementById('event-name').innerHTML=''
-    document.getElementById('event-name').innerHTML=arr_name[n];
-    document.getElementById('event-desc').innerHTML='';
-    document.getElementById('event-desc').innerHTML=arr_desc[n];
-    document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
-    document.getElementById('events').style.opacity = '1';
-    document.body.style.backdropFilter = 'blur(5px)';
+    if(event_flag==true){
+        document.getElementById('event-pic').innerHTML='<img src="neon'+q+'.jpg" style="height : 50vh; width : 35vw; border-radius : 15px;">';
+        document.getElementById('event-name').innerHTML=''
+        document.getElementById('event-name').innerHTML=arr_name[n];
+        document.getElementById('event-desc').innerHTML='';
+        document.getElementById('event-desc').innerHTML=arr_desc[n];
+        document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
+        document.getElementById('events').style.opacity = '1';
+        document.body.style.backdropFilter = 'blur(5px)';
+        event_flag=false;
+    }
 }
 function closeEvents()
 {   
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '1';
     document.getElementById('events').style.opacity = '0';
     document.body.style.backdropFilter = 'blur(0px)';
+    event_flag=true;
     
 
 }
 function openContacts() {
+    event_flag=false;
     document.getElementById('contacts').style.opacity = '1';
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '0';
     document.getElementById('events').style.opacity = '0';
@@ -30,6 +36,7 @@ function openContacts() {
 }
 function closeContacts()
 {
+    event_flag=true;
     document.getElementsByClassName('rotating-slider')[0].style.opacity = '1';
     document.getElementById('contacts').style.opacity = '0';   
     document.body.style.backdropFilter = 'blur(0px)';
